4f4b665279787a3c298f5b1bd39d8c26af1a484d
Matt Kottre <mattkottre@icloud.com>
2014-10-22 21:31:45 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-inplainsight
+InPlainSight
 ============
+
+InPlainSight is a theoretical project that I'm working that hides your information in plain sight. There are many efforts in protecting your information from unwanted access, but they don't stand a chance against government agencies and large businesses with budgets in the hundreds of millions of dollars. InPlainSight works by making connections to random websites at random times so that any third party recording and analyzing your browsing data will be unable to separate the sites you visited from the random data. THERE IS ABSOLUTELY NO GUARANTEE THAT THIS WILL IN ANY WAY PROTECT YOUR INFORMATION. THIS IS ENTIRELY A THEORETICAL PROJECT THAT I'M WORKING ON.
+
+In order to run InPlainSight:
+
+	python inplainsight.py
+
+You may need to run python3 depending on your operating system.
+
+InPlainSight only connects to web pages listed in the urls file. The default urls file currently serves as a template more than anything else. It is highly recommended to fill the file with sites that you do visit and that you don't to further obscure your data.
---
+++ inplainsight.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import http.client, random, sys, time
+
+# A connection will be made at random between the following numbers
+lower_limit = 0 # Set the lowest amount of time, in seconds, before calling random_connection()
+upper_limit = 30 # Set the highest amount of time, in seconds, before calling random_connection()
+
+def random_connection():
+	random_number = random.randint(-1,(len(urls)-1)) # Generates a random number in the range 0 to the number of urls
+	url = urls[random_number].strip('\n') # Uses the random number to choose a url and strips the new line character off of the end
+	connection = http.client.HTTPConnection(url) # Creates a connection to the url
+	connection.request("GET", "/index.html") # Makes the connection
+	r1 = connection.getresponse() # Gets the response
+	print(r1.status, r1.reason) # Prints the status of the connection
+	print(url) # Prints the url
+	connection.close() # Closes the connection
+
+urls_file = open('urls', 'r') # Opens the urls file as read-only
+urls = urls_file.readlines() # Creates an array out of the urls in the file
+while True: # Runs a loop that creates a connection on a random interval
+	interval_time = random.randint(0,30)
+	random_connection()
+	print(interval_time," seconds")
+	time.sleep(interval_time)
